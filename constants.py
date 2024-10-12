0521619f3b326f262786a9a5ec04fa9c9f03d2f5
deltag0 <110630642+deltag0@users.noreply.github.com>
2024-10-12 18:48:30 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,6 +5,6 @@ import torch
 
 
 DIR_PATH = str(Path().resolve())
-RUNTIME = 10  # maximum runtime in seconds for running model (training or testing)
+RUNTIME = 99999999  # maximum runtime in seconds for running model (training or testing)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 DATE_FORMAT = "%m-%d %H:%M:%S"
