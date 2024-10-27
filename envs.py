07eb4354032be594c8e73be207c2907e11949980
deltag0 <110630642+deltag0@users.noreply.github.com>
2024-10-26 23:19:25 -0400

    Updated envs

--- envs.py
+++ envs.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from dataclasses import dataclass
 
 import gymnasium as gym
 import torch
@@ -10,7 +9,6 @@ from sumo_rl import SumoEnvironment  # sumo_rl will be needed to setup some envi
 from constants import DIR_PATH, RUNTIME
 
 
-@dataclass
 class Four_way:
     """
     The display variable should be initialized by the user to choose
@@ -21,30 +19,59 @@ class Four_way:
     - the number of lanes
     - the max_occupancy in the lanes
     """
-    display: bool = False
+    def __init__(self, display: bool = False, lanes: int = 4, max_occupancy: int = 10):
+        self.display = display
 
-    env = SumoEnvironment(
-        net_file=os.path.join(DIR_PATH, "sumo_nets", "4_way_intersection", "4_way_map.net.xml"),
-        route_file=os.path.join(DIR_PATH, "sumo_nets", "4_way_intersection", "4_way_route.rou.xml"),
-        num_seconds=RUNTIME,
-        use_gui=display
-    )
-    lanes = 4
-    max_occupancy = 30
+        self.env = SumoEnvironment(
+            net_file=os.path.join(DIR_PATH, "sumo_nets", "4_way_intersection", "4_way_map.net.xml"),
+            route_file=os.path.join(DIR_PATH, "sumo_nets", "4_way_intersection", "4_way_route.rou.xml"),
+            num_seconds=RUNTIME,
+            use_gui=display,
+            sumo_warnings=False
+        )
+        self.lanes = lanes
+        self.max_occupancy = max_occupancy
 
 
-@dataclass
 class FourxFour:
     """
-    todo
+    The FourxFour class aggregates all necessary information for the 4x4 grid environment:
+    - the SUMO environment,
+    - the number of lanes,
+    - the maximum occupancy per lane.
+    The `display` variable is user-initialized to control GUI display.
     """
+    def __init__(self, display: bool = False, lanes: int = 4, max_occupancy: int = 10):
+        self.display = display
 
-    display: bool = False
-    env = SumoEnvironment(
-        net_file=os.path.join(DIR_PATH, "sumo_nets", "4x4_Grid", "4x4.net.xml"),
-        route_file=os.path.join(DIR_PATH, "sumo_nets", "4x4_Grid", "4x4.rou.xml"),
-        num_seconds=RUNTIME,
-        use_gui=display
-    )
-    lanes = 16
-    max_occupancy = 10
+        self.env = SumoEnvironment(
+            net_file=os.path.join(DIR_PATH, "sumo_nets", "4x4_Grid", "4x4.net.xml"),
+            route_file=os.path.join(DIR_PATH, "sumo_nets", "4x4_Grid", "4x4.rou.xml"),
+            num_seconds=RUNTIME,
+            use_gui=display,
+            sumo_warnings=False
+        )
+        self.lanes = lanes
+        self.max_occupancy = max_occupancy
+
+
+class TwoxTwo:
+    """
+    The TwoxTwo class aggregates all necessary information for the 2x2 grid environment:
+    - the SUMO environment,
+    - the number of lanes,
+    - the maximum occupancy per lane.
+    The `display` variable is user-initialized to control GUI display.
+    """
+    def __init__(self, display: bool = False, lanes: int = 4, max_occupancy: int = 10):
+        self.display = display
+
+        self.env = SumoEnvironment(
+            net_file=os.path.join(DIR_PATH, "sumo_nets", "2x2_Grid", "2x2.net.xml"),
+            route_file=os.path.join(DIR_PATH, "sumo_nets", "2x2_Grid", "2x2.rou.xml"),
+            num_seconds=RUNTIME,
+            use_gui=display,
+            sumo_warnings=False
+        )
+        self.lanes = lanes
+        self.max_occupancy = max_occupancy
